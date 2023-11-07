11d16faf60ffee2585182f4750e5630253ca49f8
VasileChira <vasilechira47@gmail.com>
2023-11-07 20:19:14 +0200

    eight commit checkout update

--- order/forms.py
+++ order/forms.py
@@ -2,27 +2,27 @@ from django import forms
 from .models import Order
 
 class OrderCreateForm(forms.ModelForm):
-	DIVISION_CHOICES = (
-		('Dhaka', 'Dhaka'),
-		('Chattagram', 'Chattagram'),
-		('Rajshahi', 'Rajshahi '),
+	country_CHOICES = (
+		('Romania', 'Romania'),
+		('Germany', 'Germany'),
+		('Italy', 'Italy '),
 	)
 
 	DISCRICT_CHOICES = (
-		('Dhaka', 'Dhaka'), 
-		('Gazipur', 'Gazipur'),
-		('Narayanganj', 'Narayanganj'),
+		('Bucharest', 'Bucharest'),
+		('Berlin', 'Berlin'),
+		('Rome', 'Rome'),
 	)
 
 	PAYMENT_METHOD_CHOICES = (
-		('Rocket', 'Rocket'),
-		('Bkash','Bkash')
+		('Pay in cash on arrival', 'Pay in cash on arrival'),
+		('Pay with cash on arrival','Pay with cash on arrival')
 	)
 
-	division = forms.ChoiceField(choices=DIVISION_CHOICES)
-	district =  forms.ChoiceField(choices=DISCRICT_CHOICES)
+	country = forms.ChoiceField(choices=country_CHOICES)
+	city =  forms.ChoiceField(choices=DISCRICT_CHOICES)
 	payment_method = forms.ChoiceField(choices=PAYMENT_METHOD_CHOICES, widget=forms.RadioSelect())
 
 	class Meta:
 		model = Order
-		fields = ['name', 'email', 'phone', 'address', 'division', 'district', 'zip_code', 'payment_method', 'account_no', 'transaction_id']
+		fields = ['name', 'email', 'phone', 'address', 'country', 'city', 'zip_code', 'payment_method', 'account_no', 'transaction_id']
